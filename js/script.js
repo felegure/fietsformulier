a8e3cacaee760c15c76ba553ac6fe7fb8f34d3ac
felegure <fnezzi@yahoo.com>
2019-10-16 15:22:58 +0200

    fiets

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
    
     $(document).ready(function () {
-      var maat1=$('maat-1');
-      var maat2=$('maat-2');
-      var maat3=$('maat-3');
-      var maat4=$('maat-4');
-      var maat5=$('maat-5');
+      var maat1=$('#maat-1');
+      var maat2=$('#maat-2');
+      var maat3=$('#maat-3');
+      var maat4=$('#maat-4');
+      var maat5=$('#maat-5');
     
       $(".stads").on('change', function(){
           // get value from selected
          var choice = $("[name=fiets-1]:checked").val();
          if(choice==="Stadfiets heren"){
           showAll();  
-          maat1.fadeOut();
-          maat1.prev().fadeOut();
-          $('#maat-1').fadeOut().prev().fadeOut();
-          $('#maat-4').fadeIn().prev().fadeIn();
-          $('#maat-5').fadeOut().prev().fadeOut();             
+          maat1.fadeOut().prev().fadeOut();
+          maat5.fadeOut().prev().fadeOut();    
          }
          if (choice==="fietsdames"){
-          showAll();  
-          $('#maat-1').fadeOut().prev().fadeOut();
-          $('#maat-4').fadeOut().prev().fadeOut();
-          $('#maat-5').fadeOut().prev().fadeOut();
+          showAll(); 
+
+          maat1.fadeOut().prev().fadeOut();
+          maat4.fadeOut().prev().fadeOut();
+          maat5.fadeOut().prev().fadeOut();
+      
          }            
       })
       $(".elek").on('change', function(){
@@ -29,15 +28,13 @@
         var choice1 = $("[name=fiets-1]:checked").val();      
         if(choice1==="elekfietsheren"){
          showAll();     
-         $('#maat-1').fadeOut().prev().fadeOut();
-         $('#maat-5').fadeOut().prev().fadeOut();
+         maat1.fadeOut().prev().fadeOut();
+         maat5.fadeOut().prev().fadeOut();
         }
         if (choice1==="elekfietsdames"){
-          showAll();
-          $('#maat-4').fadeOut();
-          $('#maat-4').prev().fadeOut();
-          $('#maat-5').fadeOut();
-          $('#maat-5').prev().fadeOut();
+          showAll(); 
+          maat4.fadeOut().prev().fadeOut();
+          maat5.fadeOut().prev().fadeOut();
          }  
        
      })
